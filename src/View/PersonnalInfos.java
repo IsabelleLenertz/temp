1d56020f5a5ce73d8198e88fc6117e7fba5a79ee
Valentin Bloch <v.bloch@unistra.fr>
2020-11-20 16:25:47 +0100

    add gitignore

---
+++ src/View/PersonnalInfos.java
@@ -0,0 +1,36 @@
+package View;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class PersonnalInfos extends SideInfos {
+
+    /**
+     * Default constructor
+     */
+    public PersonnalInfos() {
+    }
+
+    /**
+     *
+     */
+    private ArrayList<String> jobs;
+
+    /**
+     *
+     */
+    private String images;
+
+    /**
+     * @param name
+     * @param desc
+     * @param jobs
+     * @param img
+     */
+    public void Personnalnofs(String name, String desc, ArrayList<String> jobs, String img) {
+        // TODO implement here
+    }
+
+}
---
+++ src/View/RelicInfo.java
@@ -0,0 +1,23 @@
+package View;
+
+/**
+ *
+ */
+public class RelicInfo extends SideInfos {
+
+    /**
+     * Default constructor
+     */
+    public RelicInfo() {
+    }
+
+    /**
+     * @param name
+     * @param desc
+     * @param img
+     */
+    public void ReliqueInfos(String name, String desc, String img) {
+        // TODO implement here
+    }
+
+}
