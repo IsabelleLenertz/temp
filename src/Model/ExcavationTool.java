5c21fb51454d8d5a71267887e405cf7994d68629
Valentin Bloch <v.bloch@unistra.fr>
2020-11-28 14:36:27 +0100

    model update

--- src/Model/ExcavationTool.java
+++ src/Model/ExcavationTool.java
@@ -1,30 +1,61 @@
 package Model;
 
-import java.util.*;
-
 /**
  * 
  */
 public class ExcavationTool {
 
+    private String name;
+    private String desc;
+    private JobEnum requirements;
+    private boolean isHighligt;
+    private State status;
     /**
      * Default constructor
      */
     public ExcavationTool() {
     }
 
-    /**
-     * 
-     */
-    private String name;
+    public ExcavationTool(String name, String desc, JobEnum requirements) {
+        this.name = name;
+        this.desc = desc;
+        this.requirements = requirements;
+        this.isHighligt = false;
+        this.status = State.UNSELECT;
+    }
 
-    /**
-     * 
-     */
-    private String desc;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    /**
-     * 
-     */
-    private JobEnum requirements;
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public JobEnum getRequirements() {
+        return requirements;
+    }
+    public void setRequirements(JobEnum requirements) {
+        this.requirements = requirements;
+    }
+
+    public boolean isHighligt() {
+        return isHighligt;
+    }
+    public void setHighligt(boolean highligt) {
+        isHighligt = highligt;
+    }
+
+    public State getStatus() {
+        return status;
+    }
+    public void setStatus(State status) {
+        this.status = status;
+    }
 }
--- src/Model/GameModel.java
+++ src/Model/GameModel.java
@@ -7,10 +7,23 @@ import java.util.*;
  */
 public class GameModel {
 
+    private ArrayList<Relic> relics;
+    private ArrayList<Person> personList;
+    private ArrayList<ExcavationTool> excavationTools;
+    private ArrayList<ExploitationTool> exploitationTool;
+    private String rapport;
+
     /**
      * Default constructor
      */
     public GameModel() {
+    }
+
+    public GameModel(ArrayList<Relic> relic, ArrayList<Person> person, ArrayList<ExcavationTool> excavationTool, ArrayList<ExploitationTool> exploitationTool) {
+        this.relics = relic;
+        this.personList = person;
+        this.excavationTools = excavationTool;
+        this.exploitationTool = exploitationTool;
     }
 
     /**
@@ -45,4 +58,38 @@ public class GameModel {
         return null;
     }
 
+    public ArrayList<Relic> getRelic() {
+        return relics;
+    }
+    public void setRelic(ArrayList<Relic>  relic) {
+        this.relics = relic;
+    }
+
+    public ArrayList<Person> getPerson() {
+        return this.personList;
+    }
+    public void setPerson(ArrayList<Person> person) {
+        this.personList = person;
+    }
+
+    public ArrayList<ExcavationTool>getExcavationTool() {
+        return excavationTools;
+    }
+    public void setExcavationTool(ArrayList<ExcavationTool> excavationTool) {
+        this.excavationTools = excavationTool;
+    }
+
+    public ArrayList<ExploitationTool> getExploitationTool() {
+        return exploitationTool;
+    }
+    public void setExploitationTool(ArrayList<ExploitationTool> exploitationTool) {
+        this.exploitationTool = exploitationTool;
+    }
+
+    public String getRapport() {
+        return rapport;
+    }
+    public void setRapport(String rapport) {
+        this.rapport = rapport;
+    }
 }
