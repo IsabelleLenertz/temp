71ec9b5fa6359f9ee3777eeb2cbcdeeb1255574a
Pierre Lenertz <pierre.lenertz@etu.unistra.fr>
2020-12-05 19:43:10 +0100

    Implementing the resource restrictions when selecting

--- src/Model/GameModel.java
+++ src/Model/GameModel.java
@@ -10,7 +10,7 @@ import java.util.*;
  * 
  */
 public class GameModel implements Subject {
-
+    private final int maxHireNumber = 10; // TODO to balance for the game scenario
     private ArrayList<Relic> relics;
     private ArrayList<Person> personList;
     private ArrayList<ExcavationTool> excavationTools;
@@ -119,12 +119,19 @@ public class GameModel implements Subject {
 
         switch (currentStage){
             case 0:
+                // Main menu
                 break;
             case 1:
+                // team selection
                 break;
             case 2:
+                // excavation
                 break;
             case 3:
+                // exploitation
+                break;
+            case 4:
+                // results
                 break;
         }
         this.notifyObservers();
@@ -138,5 +145,243 @@ public class GameModel implements Subject {
     @Override
     public ArrayList<Observer> getObservers() {
         return this.observers;
+    }
+
+    public ArrayList<Person> getSelectedPersons() {
+        ArrayList<Person> selectedP = new ArrayList<Person>();
+        for (Person pers : personList) {
+            if (pers.getStatus() == State.SELECTED) {
+                selectedP.add(pers);
+            }
+        }
+        return selectedP;
+    }
+
+    private ArrayList<ExcavationTool> getSelectedExcavationTools() {
+        ArrayList<ExcavationTool> selectedT = new ArrayList<ExcavationTool>();
+        for (ExcavationTool tool : excavationTools) {
+            if (tool.getStatus() == State.SELECTED) {
+                selectedT.add(tool);
+            }
+        }
+        return selectedT;
+    }
+
+    private ArrayList<ExploitationTool> getSelectedExploitationTools() {
+        ArrayList<ExploitationTool> selectedT = new ArrayList<ExploitationTool>();
+        for (ExploitationTool tool : exploitationTools) {
+            if (tool.getStatus() == State.SELECTED) {
+                selectedT.add(tool);
+            }
+        }
+        return selectedT;
+    }
+
+    private ArrayList<JobEnum> getUsableJobs() {
+        ArrayList<JobEnum> jobs = new ArrayList<JobEnum>();
+        for (Person pers : getSelectedPersons()) {
+            for (JobEnum job : pers.getJobs()) {
+                jobs.add(job);
+            }
+        }
+        return jobs;
+    }
+
+    private ArrayList<JobEnum> getUsedJobsForExcavation() {
+        ArrayList<JobEnum> jobs = new ArrayList<JobEnum>();
+        for (ExcavationTool excaTool : getSelectedExcavationTools()) {
+            for (JobEnum job : excaTool.getRequirements()) {
+                jobs.add(job);
+            }
+        }
+        return jobs;
+    }
+
+    private ArrayList<JobEnum> getUsedJobsForExploitation() {
+        ArrayList<JobEnum> jobs = new ArrayList<JobEnum>();
+        for (ExploitationTool exploTool : getSelectedExploitationTools()) {
+            for (JobEnum job : exploTool.getRequirement()) {
+                jobs.add(job);
+            }
+        }
+        return jobs;
+    }
+
+    public ArrayList<JobEnum> getAvailableExcavationJobs() {
+        ArrayList<JobEnum> availableJobs = getUsableJobs();
+        ArrayList<JobEnum> usedJobs = getUsedJobsForExcavation();
+        // or each used job, we need to remove 1 of the resource in the available jobs
+        for (int i = 0; i < usedJobs.size(); i++) {
+            for (int j = 0; j < availableJobs.size(); j++) {
+                if (availableJobs.get(j) == usedJobs.get(i)) {
+                    availableJobs.remove(j);
+                    break;
+                }
+            }
+        }
+        return availableJobs;
+    }
+
+    public ArrayList<JobEnum> getAvailableExploitationJobs() {
+        ArrayList<JobEnum> availableJobs = getUsableJobs();
+        ArrayList<JobEnum> usedJobs = getUsedJobsForExploitation();
+        // or each used job, we need to remove 1 of the resource in the available jobs
+        for (int i = 0; i < usedJobs.size(); i++) {
+            for (int j = 0; j < availableJobs.size(); j++) {
+                if (availableJobs.get(j) == usedJobs.get(i)) {
+                    availableJobs.remove(j);
+                    break;
+                }
+            }
+        }
+        return availableJobs;
+    }
+
+    public int getCurrentStage() {
+        return currentStage;
+    }
+
+    public int getAvailableHireCount() {
+        return maxHireNumber - getSelectedPersons().size();
+    }
+
+    public void selectPerson(String name) {
+        // selecting the person
+        for (Person p : personList) {
+            if (name.equals(p.getName())) {
+                p.setStatus(State.SELECTED);
+                break;
+            }
+        }
+        // updating the available persons
+        if (getAvailableHireCount() == 0) {
+            for (Person pers : personList) {
+                if (pers.getStatus() == State.UNSELECTED) {
+                    pers.setStatus(State.UNAVAILABLE);
+                }
+            }
+        }
+        // sending an update to views
+        notifyObservers();
+    }
+
+    public void unselectPerson(String name) {
+        // unselecting the person
+        for (Person p : personList) {
+            if (name.equals(p.getName())) {
+                p.setStatus(State.UNSELECTED);
+                break;
+            }
+        }
+        // updating the available persons
+        if (getAvailableHireCount() != 0) {
+            for (Person pers : personList) {
+                if (pers.getStatus() == State.UNAVAILABLE) {
+                    pers.setStatus(State.UNSELECTED);
+                }
+            }
+        }
+        // sending an update to views
+        notifyObservers();
+    }
+
+    public void selectExcavationTool(String name) {
+        // selecting the tool
+        for (ExcavationTool p : excavationTools) {
+            if (name.equals(p.getName())) {
+                p.setStatus(State.SELECTED);
+                break;
+            }
+        }
+        // updating available tools
+        ArrayList<JobEnum> availableJobs = getAvailableExcavationJobs();
+        for (ExcavationTool tool : excavationTools) {
+            if (tool.getStatus() == State.UNSELECTED) {
+                boolean requirementMet = true;
+                ArrayList<JobEnum> requiredJobs = tool.getRequirements();
+                for (int i = 0; i < requiredJobs.size() && requirementMet; i++) {
+                    requirementMet = availableJobs.contains(requiredJobs.get(i));
+                }
+                if (!requirementMet)
+                    tool.setStatus(State.UNAVAILABLE);
+            }
+        }
+        // sending an update to views
+        notifyObservers();
+    }
+
+    public void unselectExcavationTool(String name) {
+        // unselecting the tool
+        for (ExcavationTool p : excavationTools) {
+            if (name.equals(p.getName())) {
+                p.setStatus(State.UNSELECTED);
+                break;
+            }
+        }
+        // updating available tools
+        ArrayList<JobEnum> availableJobs = getAvailableExcavationJobs();
+        for (ExcavationTool tool : excavationTools) {
+            if (tool.getStatus() == State.UNAVAILABLE) {
+                boolean requirementMet = true;
+                ArrayList<JobEnum> requiredJobs = tool.getRequirements();
+                for (int i = 0; i < requiredJobs.size() && requirementMet; i++) {
+                    requirementMet = availableJobs.contains(requiredJobs.get(i));
+                }
+                if (requirementMet)
+                    tool.setStatus(State.UNSELECTED);
+            }
+        }
+        // sending an update to views
+        notifyObservers();
+    }
+
+    public void selectExploitationTool(String name) {
+        // selecting the tool
+        for (ExploitationTool p : exploitationTools) {
+            if (name.equals(p.getName())) {
+                p.setStatus(State.SELECTED);
+                break;
+            }
+        }
+        // updating available tools
+        ArrayList<JobEnum> availableJobs = getAvailableExploitationJobs();
+        for (ExploitationTool tool : exploitationTools) {
+            if (tool.getStatus() == State.UNSELECTED) {
+                boolean requirementMet = true;
+                ArrayList<JobEnum> requiredJobs = tool.getRequirement();
+                for (int i = 0; i < requiredJobs.size() && requirementMet; i++) {
+                    requirementMet = availableJobs.contains(requiredJobs.get(i));
+                }
+                if (!requirementMet)
+                    tool.setStatus(State.UNAVAILABLE);
+            }
+        }
+        // sending an update to views
+        notifyObservers();
+    }
+
+    public void unselectExploitationTool(String name) {
+        // selecting the tool
+        for (ExploitationTool p : exploitationTools) {
+            if (name.equals(p.getName())) {
+                p.setStatus(State.UNSELECTED);
+                break;
+            }
+        }
+        // updating available tools
+        ArrayList<JobEnum> availableJobs = getAvailableExploitationJobs();
+        for (ExploitationTool tool : exploitationTools) {
+            if (tool.getStatus() == State.UNAVAILABLE) {
+                boolean requirementMet = true;
+                ArrayList<JobEnum> requiredJobs = tool.getRequirement();
+                for (int i = 0; i < requiredJobs.size() && requirementMet; i++) {
+                    requirementMet = availableJobs.contains(requiredJobs.get(i));
+                }
+                if (requirementMet)
+                    tool.setStatus(State.UNSELECTED);
+            }
+        }
+        // sending an update to views
+        notifyObservers();
     }
 }
