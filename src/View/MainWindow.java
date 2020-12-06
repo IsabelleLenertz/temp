de08486d228909a376cff15da648ca4a29b87b9c
Pierre Lenertz <pierre.lenertz@etu.unistra.fr>
2020-12-06 15:42:26 +0100

    Implementing the main window used to display views

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -1,19 +1,58 @@
 package View;
 
-public class MainWindow {
+import Control.GameControleur;
+import observer.Observer;
 
-    StageView stage;
+import javax.swing.*;
 
-    public MainWindow(StageView stage)
+public class MainWindow extends JFrame implements Observer {
+
+    private JPanel stagePanel;
+    private GameControleur g;
+    private StageView stage;
+    private int curentStage = -1;
+
+    public MainWindow(GameControleur g)
     {
-        this.stage = stage;
+        this.g = g;
+        this.stagePanel = new JPanel();
+        this.add(stagePanel);
+        update();
     }
 
-    public StageView getStage() {
-        return stage;
-    }
+    @Override
+    public void update() {
+        int stageNumber = g.getGameStage();
 
-    public void setStage(StageView stage) {
-        this.stage = stage;
+        if (stageNumber != curentStage) {
+            curentStage = stageNumber;
+            this.stagePanel.removeAll();
+            switch (curentStage) {
+                case 0:
+                    this.stage = null;
+                    stagePanel.add(new MainMenuView(g));
+                    break;
+                case 1:
+                    this.stage = new TeamView(g);
+                    stagePanel.add(stage);
+                    break;
+                case 2:
+                    this.stage = new TeamView(g);
+                    stagePanel.add(stage);
+                    break;
+                case 3:
+                    this.stage = new TeamView(g);
+                    stagePanel.add(stage);
+                    break;
+                case 4:
+                    this.stage = null;
+                    // stagePanel.add(new ResultsView(g));
+                    break;
+            }
+            stagePanel.revalidate();
+        }
+        if (stage != null) {
+            stage.update();
+        }
     }
 }
