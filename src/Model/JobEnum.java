b2363a9ad81a2db05de8e8ec43f6163e949a5c7b
Valentin Bloch <v.bloch@unistra.fr>
2020-12-10 13:41:22 +0100

    add scenario

--- src/Model/JobEnum.java
+++ src/Model/JobEnum.java
@@ -4,5 +4,19 @@ package Model;
  * 
  */
 public enum JobEnum {
-
+    directeur_de_chantier,
+    bénévoles,
+    étudiants_en_archéologie,
+    géographe,
+    géomaticien,
+    données_géographiques,
+    céramologue,
+    médiéviste,
+    antiquisant,
+    paléontologue,
+    technicien_de_fouille,
+    scientifique_de_laboratoire,
+    conducteur_engin_de_chantier,
+    archéologue,
+    géoarchéologue
 }
